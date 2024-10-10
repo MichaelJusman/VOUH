47f005dd2875dac7015fd6ef1ce8d72a819d920c
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2024-10-10 22:22:43 +1100

    Movement scripts

--- Assets/Scripts/PlayerInputs.cs
+++ Assets/Scripts/PlayerInputs.cs
@@ -2,13 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Obvious.Soap;
+using Unity.VisualScripting;
 
 public class PlayerInputs : MonoBehaviour
 {
-    [SerializeField] private Vector3Variable _inputs;
+    [Header("Movement")]
+    [SerializeField] private Vector3Variable _movementInput;
+    [SerializeField] private BoolVariable _dashInput;
+    [SerializeField] private BoolVariable _slideInput;
+
+    [Header("Tap")]
+    [SerializeField] private FloatVariable _tapInput;
+    [SerializeField] private float tapTreshold;
+    [SerializeField] private float movementKeyTimer = 0;
+    [SerializeField] private bool isMovementKeyHeld = false;
+
 
     private void Update()
     {
-        _inputs.Value = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // Movement inputs
+        _movementInput.Value = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        // Tap detection
+        if (_movementInput.Value.magnitude > 0) //If detects any movement input
+        {
+            if (!isMovementKeyHeld)
+            {
+                isMovementKeyHeld = true;
+                movementKeyTimer = 0;
+            }
+            else { movementKeyTimer += Time.deltaTime; } //Start timer
+        }
+        else if(isMovementKeyHeld) //Release
+        {
+            if(movementKeyTimer <= tapTreshold)
+            {
+                _tapInput.Value = 1;
+            }
+            else
+            {
+                _tapInput.Value = 0;
+            }
+            isMovementKeyHeld = false;
+
+        }
+
+        //Dash input
+        _dashInput.Value = Input.GetKeyDown(KeyCode.Space);
+
+        //Slide input
+        _slideInput.Value = Input.GetKey(KeyCode.LeftShift);
+        
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Vector3Variable _inputs;
-    [SerializeField] private FloatVariable _tempEnergy;
+    [Header("Inputs")]
+    [SerializeField] private Vector3Variable _movementInputs;
+    [SerializeField] private BoolVariable _dashInput;
+    [SerializeField] private BoolVariable _slideInput;
+    [SerializeField] private FloatVariable _tapInput;
+
+    [Header("Player Resources")]
+    [SerializeField] private FloatVariable _energy;
 
     [Header("Speed Variables")]
     [SerializeField] private float currentSpeed;
@@ -19,38 +25,51 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float tapCost;
 
     [Header("Dash")]
+    [SerializeField] private bool isDashing = false;
     [SerializeField] private FloatVariable _dash;
     [SerializeField] private float dashDecay;
     [SerializeField] private float dashCost;
+    [SerializeField] private float dashDuration;
+    private Coroutine dashCoroutine;
 
     [Header("Slide")]
     [SerializeField] private bool isSliding = false;
+    [SerializeField] private FloatVariable _kineticSlideInterval;
     [SerializeField] private float slideTreshold;
     [SerializeField] private float slideFriction;
-    [SerializeField] private FloatVariable slideEnergyRegen;
+    [SerializeField] private FloatVariable _slideEnergyRegen;
+    private Coroutine slideCoroutine;
 
 
 
     private void Update()
     {
         HandleMovement();
+        HandleDash();
+        HandleSliding();
     }
 
     void HandleMovement()
     {
-        if(_inputs.Value.magnitude > 0)
+        if(_movementInputs.Value.magnitude > 0)
         {
-            if(Input.GetKeyDown(KeyCode.A) ||  Input.GetKeyDown(KeyCode.D))
+            if(_tapInput.Value > 0)
             {
-                if(slideEnergyRegen >= tapCost)
+                if(_energy.Value >= tapCost)
                 {
+                    Vector3 dashDirection = _movementInputs.Value.normalized;
                     currentSpeed = Mathf.Min(currentSpeed + tapSpeedBoost, _speedMax);
-                    _tempEnergy.Value -= tapCost;
+                    transform.position += dashDirection * tapSpeedBoost;
+                    _energy.Value -= tapCost;
+                    _tapInput.Value = 0;
                 }
             }
 
-            //Speed Buildup
-            currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, _speedMax);
+            if(!isDashing)
+            {
+                currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, _speedMax);
+            }
+            
         }
         else if(!isSliding)
         {
@@ -59,6 +78,70 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //Apply Movement
-        transform.position += _inputs.Value * currentSpeed * Time.deltaTime;
+        transform.position += _movementInputs.Value * currentSpeed * Time.deltaTime;
+    }
+
+    void HandleDash()
+    {
+        if(_dashInput && !isDashing && _energy.Value >= dashCost)
+        {
+            dashCoroutine = StartCoroutine(Dash());
+            _energy.Value -= dashCost;
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+
+        currentSpeed += _dash;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        if(currentSpeed > _speedMax)
+        {
+            currentSpeed = _speedMax;
+        }
+
+        isDashing = false;
+    }
+
+    void HandleSliding()
+    {
+        if(currentSpeed > slideTreshold && _slideInput && !isSliding)
+        {
+            isSliding = true;
+            slideCoroutine = StartCoroutine(KineticSlide());
+        }
+
+        if(isSliding)
+        {
+            currentSpeed = Mathf.Max(currentSpeed - slideFriction * Time.deltaTime, 0);
+
+            if (currentSpeed <= 0) // Stop sliding when speed is 0
+            {
+                isSliding = false;
+                StopCoroutine(slideCoroutine);
+            }
+
+            if (!_slideInput) // Stop sliding when the key is released
+            {
+                isSliding = false;
+                StopCoroutine(slideCoroutine);
+            }
+        }
+    }
+
+    IEnumerator KineticSlide()
+    {
+        while(isSliding)
+        {
+            if(_movementInputs.Value.magnitude > 0)
+            {
+                _energy.Value += _slideEnergyRegen;
+            }
+
+            yield return new WaitForSeconds(_kineticSlideInterval);
+        }
     }
 }
