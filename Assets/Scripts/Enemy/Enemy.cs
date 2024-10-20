f4578420fe2af0c1076129d73e6981f083d9434a
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2024-10-20 23:11:59 +1100

    basic enemy chase working

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,12 +6,29 @@ using UnityEngine;
 
 public abstract class Enemy : GameBehaviour
 {
-    [SerializeField] private float damage;
-    [SerializeField] private float speed;
+    [SerializeField] protected float damage;
+    [SerializeField] protected float speed;
     [SerializeField] private FloatVariable _roundMultiplier;
 
     [Header("Events")]
     [SerializeField] private ScriptableEventInt _onEnemyHitPlayer;
+
+    [Header("Player References")]
+    [SerializeField] private Vector3Variable _playerPos;
+    protected Vector3 direction;
+
+    private void Update()
+    {
+        UpdateDirection();
+        EnemyBehaviour();
+    }
+
+    protected abstract void EnemyBehaviour();
+
+    void UpdateDirection()
+    {
+        direction = (_playerPos.Value - transform.position).normalized;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
